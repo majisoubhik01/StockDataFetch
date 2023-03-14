9021344a9712f491ecd4c9b7422a1b6b307efa91
Soubhik Maji <67087252+majisoubhik01@users.noreply.github.com>
2023-03-14 08:47:10 +0530

    Update stockfetch.py

--- stockfetch.py
+++ stockfetch.py
@@ -7,23 +7,24 @@ import streamlit as st
 
 sel = st.selectbox('Pick One',['IT','AUTO'])
 
-if sel == "IT":
-  tickers = np.array(['TCS.NS','TECHM.NS','WIPRO.NS',
+if st.button("Get Data"):
+  if sel == "IT":
+    tickers = np.array(['TCS.NS','TECHM.NS','WIPRO.NS',
                     'HCLTECH.NS','MPHASIS.NS','INFY.NS',
                     'LT.NS', 'SUBEXLTD.NS', 'NUCLEUS.NS'])
-elif sel == "AUTO":
-  tickers = np.array(['M&M.NS','MARUTI.NS','TATAMOTORS.NS','EICHERMOT.NS',
+  elif sel == "AUTO":
+    tickers = np.array(['M&M.NS','MARUTI.NS','TATAMOTORS.NS','EICHERMOT.NS',
                     'BAJAJ-AUTO.NS','HEROMOTOCO.NS','TIINDIA.NS',
                     'TVSMOTOR.NS', 'ASHOKLEY.NS', 'BHARATFORG.NS'])
+  
+  yahoo_financials = YahooFinancials(tickers)
 
-yahoo_financials = YahooFinancials(tickers)
-
-#start_date = st.date_input('Enter Start Date')
-#st.write(type(start_date))
-data = yahoo_financials.get_historical_price_data(start_date='2018-01-01', 
+  #start_date = st.date_input('Enter Start Date')
+  #st.write(type(start_date))
+  data = yahoo_financials.get_historical_price_data(start_date='2018-01-01', 
                                                   end_date='2022-12-31', 
                                                   time_interval='daily')
 
-prices_df = pd.DataFrame({a: {x['formatted_date']: x['close'] for x in data[a]['prices']} for a in tickers})
+  prices_df = pd.DataFrame({a: {x['formatted_date']: x['close'] for x in data[a]['prices']} for a in tickers})
 
-st.table(prices_df.head())
+  st.table(prices_df.head())
